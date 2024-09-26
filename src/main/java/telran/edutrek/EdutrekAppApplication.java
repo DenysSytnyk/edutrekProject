0364ccc115c5183a2c021086e027506877cb28be
Denys Sytnyk <denis.sytnyk@gmail.com>
2024-09-26 05:17:07 +0300

    Init commit

---
+++ src/main/java/telran/edutrek/EdutrekAppApplication.java
@@ -0,0 +1,13 @@
+package telran.edutrek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdutrekAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdutrekAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/telran/edutrek/EdutrekAppApplicationTests.java
@@ -0,0 +1,13 @@
+package telran.edutrek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdutrekAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
